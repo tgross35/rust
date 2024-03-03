95b7f8a0908f28c0613911ceada6470b7b9e1f4b
Trevor Gross <tmgross@umich.edu>
2024-03-02 21:45:23 -0500

    Add feature gates for `f16` and `f128`
    Includes related tests and documentation pages.

---
+++ tests/ui/feature-gates/feature-gate-f128.rs
@@ -0,0 +1,36 @@
+#![allow(unused)]
+
+// ensure gating for primitive use
+mod m1 {
+    const A: f128 = 10.0; //~ ERROR the feature `f128` is unstable
+
+    pub fn main() {
+        let a: f128 = 100.0; //~ ERROR the feature `f128` is unstable
+        let b = 0.0f128; //~ ERROR the feature `f128` is unstable
+        foo(1.23);
+    }
+
+    fn foo(a: f128) {} //~ ERROR the feature `f128` is unstable
+
+    struct Bar {
+        a: f128, //~ ERROR the feature `f128` is unstable
+    }
+}
+
+// ensure we don't restrict name as an identifier or custom types
+mod mod1 {
+    #[allow(non_camel_case_types)]
+    struct f128 {
+        a: i32,
+    }
+
+    fn foo() {
+        let f128 = ();
+    }
+
+    fn bar(a: f128) -> i32 {
+        a.a
+    }
+}
+
+fn main() {}
